126757f3ff5c2b42e939988f0243f22dc5013c74
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-08-27 21:20:24 +0100

    experimental app to search for difference-preserving binary operators of low order

---
+++ src/main/scala/com/fdilke/bewl/apps/DPFunctions.scala
@@ -0,0 +1,46 @@
+package com.fdilke.bewl.apps
+
+import com.fdilke.bewl.fsets.FiniteSets.{DOT, FiniteSetsDot}
+
+// Originally for finding 'difference-preserving binary operators'.
+// It doesn't find any up to n=3.
+// Also serves as a cue to fix allMaps() which seems to break on 4 ^ 16, never enumerating anything.
+
+object DPFunctions extends App {
+
+  def countDP(n: Int) = {
+    val sizeN = FiniteSetsDot(0 until n)
+    val pairs = sizeN x sizeN
+    var numExamined = 0
+
+    def isDifferencePreserving(f: ((Int, Int)) => Int) = (
+      for (x <- sizeN;
+           y <- sizeN if x != y;
+           p <- sizeN;
+           q <- sizeN if p != q)
+      yield f((x, p)) != f((y, q))
+      ).forall(identity)
+
+    def showIfDifferencePreserving(f: ((Int, Int)) => Int) = {
+      numExamined = numExamined + 1
+      if (numExamined % 100 == 0) {
+        println(s"[$numExamined]")
+      }
+      if (isDifferencePreserving(f)) {
+        for (x <- sizeN;
+             y <- sizeN) {
+          print(s"$x$y=${f((x,y))} ")
+        }
+        println
+        true
+      } else false
+    }
+
+    (sizeN ^ pairs).count(showIfDifferencePreserving)
+  }
+
+  println("DP:")
+  for (n <- 0 to 4) {
+    println(s"$n -> ${countDP(n)}")
+  }
+}
