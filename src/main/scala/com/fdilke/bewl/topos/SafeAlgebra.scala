08e9e71bcd4e60b878f47950d74d30a58c2180eb
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-10-12 00:07:24 +0100

    initial SafeAlgebra code

---
+++ src/main/scala/com/fdilke/bewl/topos/SafeAlgebra.scala
@@ -0,0 +1,22 @@
+package com.fdilke.bewl.topos
+
+trait SafeAlgebra { topos: Topos =>
+
+  trait SafeArgument {
+    type TYPE <: ELEMENT
+    val star: STAR[TYPE]
+  }
+
+  abstract class SafeTuple[ST <: SafeTuple[ST]](args: Seq[SafeArgument]) {
+    class Assignments
+  }
+
+  abstract class SafeOperator[
+    X <: ELEMENT,
+    ST <: SafeTuple[ST]
+  ] (
+    tuple: ST
+  ) {
+    def eval(assignments: ST#Assignments)
+  }
+}
