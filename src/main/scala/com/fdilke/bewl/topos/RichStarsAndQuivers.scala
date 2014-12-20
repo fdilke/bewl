f904b0d3b501d0e2c36a11bac87fed35cb3e588b
Felix Dilke <fdilkeuk@yahoo.co.uk>
2014-12-20 12:53:09 +0000

    yet more streamlined definition of ∃

--- src/main/scala/com/fdilke/bewl/topos/RichStarsAndQuivers.scala
+++ src/main/scala/com/fdilke/bewl/topos/RichStarsAndQuivers.scala
@@ -34,20 +34,23 @@ trait RichStarsAndQuivers { topos: BaseTopos =>
       }
 
     lazy val ∃ = RichStarsAndQuivers.this.∃(star)
+
+    def forAll[R <: ELEMENT](target: STAR[R])(g: (R, X) => TRUTH): QUIVER[R, TRUTH] =
+      ∀ o power.transpose(
+        (target x star).biQuiver(omega)(g)
+      )
   }
 
   implicit def enrich[X <: ELEMENT](star: STAR[X]) = new RichStar(star)
 
   private def ∃[X <: ELEMENT](star: STAR[X]) =
-    omega.∀ o omega.power.transpose(
-      (star.power x omega).biQuiver(omega) {
+    omega.forAll(star.power) {
         case (f, w) =>
-          TruthObject.implies(BiQuiver(star.power x omega, star.∀ o star.power.transpose(
-            (star.power x omega x star).biQuiver(omega) {
+          TruthObject.implies(BiQuiver(star.power x omega, star.forAll(star.power x omega) {
               case ((f, w), x) => TruthObject.implies(f(x), w)
             }
-          ))(f, w), w)
-        })
+          )(f, w), w)
+        }
 
   object TruthObject { // TODO: This should eventually express omega as a complete Heyting algebra
     lazy val omegaSquared = omega x omega
