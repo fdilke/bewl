85989aefa2af5d0dffb5bcd7fed1c31f8c7a5c47
Felix Dilke <fdilkeuk@yahoo.co.uk>
2016-05-29 19:32:42 +0100

    initial Yang-Baxter code

---
+++ src/main/scala/com/fdilke/bewl/apps/YangBaxter.scala
@@ -0,0 +1,24 @@
+package com.fdilke.bewl.apps
+
+import com.fdilke.bewl.fsets.FiniteSets._
+import com.fdilke.bewl.fsets.FiniteSetsUtilities._
+
+object YangBaxter extends App {
+  val B = dot(1,2)
+  val BxB = B x B
+  val BxBxB = BxB x B
+  val isSolution =
+    (BxB > BxB)(omega) { f =>
+      val xx = (BxB > ((BxB.π1 o BxBxB.π0) x BxBxB.π1) )(f)
+
+      val hh: (((Int x Int) x Int) → (Int x Int)) = (BxB > BxBxB.π0)(f)
+//      val _f_1 = (BxB > BxBxB.π0)(f) x BxBxB.π1
+
+//      val _1_f = (
+//        (BxB.π0 o BxBxB.π0) x (BxB.π0 o xx)
+//       ) x (
+//          BxB.π1 o xx
+//       )
+      ???
+    }
+}
