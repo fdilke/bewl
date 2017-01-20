b9713303e2d19157f14e4acabca0be7e0b0debc5
felix <felix.dilke@springer.com>
2017-01-20 13:45:26 +0000

    utility to iterate to a fixed point (for fast coequalizers)

---
+++ src/main/scala/com/fdilke/bewl/helper/IterateToFixed.scala
@@ -0,0 +1,23 @@
+package com.fdilke.bewl.helper
+
+import scala.annotation.tailrec
+
+object IterateToFixed {
+
+  def apply[T](
+    initial: T
+  ) (
+    fn: T => T
+  ) = {
+    @tailrec def iterateOn(
+      value: T
+    ): T = {
+      val next = fn(value)
+      if (next == value)
+        value
+      else
+        iterateOn(next)
+    }
+    iterateOn(initial)
+  }
+}
---
+++ src/test/scala/com/fdilke/bewl/helper/IterateToFixedTest.scala
@@ -0,0 +1,28 @@
+package com.fdilke.bewl.helper
+
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers._
+
+import scala.collection.mutable.ListBuffer
+
+class IterateToFixedTest extends FreeSpec {
+
+  "Iterating to a fixed point"- {
+    "works" in {
+      val argumentsSeen = new ListBuffer[Int]
+      def collatz(n: Int) = {
+        argumentsSeen += n
+        if (n == 1)
+          1
+        else if (n % 2 == 0)
+          n/2
+        else
+          3*n + 1
+      }
+      IterateToFixed(6)(collatz) shouldBe 1
+      argumentsSeen.toList shouldBe Seq(
+        6, 3, 10, 5, 16, 8, 4, 2, 1
+      )
+    }
+  }
+}
