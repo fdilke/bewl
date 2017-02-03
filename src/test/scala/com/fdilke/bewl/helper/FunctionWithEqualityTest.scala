945b89e9db8489014b963f27a1185484c3839b6f
Felix <felix.dilke@springernature.com>
2017-02-03 13:34:31 +0000

    tidying up

--- src/test/scala/com/fdilke/bewl/helper/FunctionWithEqualityTest.scala
+++ src/test/scala/com/fdilke/bewl/helper/FunctionWithEqualityTest.scala
@@ -3,14 +3,12 @@ package com.fdilke.bewl.helper
 import org.scalatest.Matchers._
 import org.scalatest.{FunSpec, Matchers}
 
-// User: Felix Date: 07/06/2014 Time: 10:56
-
 class FunctionWithEqualityTest extends FunSpec {
   describe("functions with equality") {
     it("pass through the function argument and return value") {
       val f = (x: Int) => x + x
       val domain = Set(1,2,3)
-      val fE = FunctionWithEquality(domain,f)
+      val fE = FunctionWithEquality(domain, f)
       fE(1) shouldBe 2
       fE(2) shouldBe 4
     }
@@ -22,15 +20,16 @@ class FunctionWithEqualityTest extends FunSpec {
       val g = (x: Int) => 2 * x
       (f == g) shouldBe false
       val domain = Set(1,2,3)
-      (FunctionWithEquality(domain,f) ==
-        FunctionWithEquality(domain, g)) shouldBe true
+      (FunctionWithEquality(domain, f) ==
+        FunctionWithEquality(domain, g)
+      ) shouldBe true
     }
 
     it("identify functions according to the domain") {
       val f = (x: Int) => x * x + 1
       val g = (x: Int) => 2
       val domain = Set(1,-1)
-      (FunctionWithEquality(domain,f) ==
+      (FunctionWithEquality(domain, f) ==
         FunctionWithEquality(domain, g)) shouldBe true
       val biggerDomain = Set(1,-1,3)
       (FunctionWithEquality(biggerDomain,f) ==
