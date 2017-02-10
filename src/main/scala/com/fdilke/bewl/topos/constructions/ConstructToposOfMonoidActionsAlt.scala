1f1359204bae188768e4111a99e1929868e10a19
Felix <felix.dilke@springernature.com>
2017-02-10 18:30:26 +0000

    fast actions: skeleton definitions for main classes

--- src/main/scala/com/fdilke/bewl/topos/constructions/ConstructToposOfMonoidActionsAlt.scala
+++ src/main/scala/com/fdilke/bewl/topos/constructions/ConstructToposOfMonoidActionsAlt.scala
@@ -18,8 +18,105 @@ trait ConstructToposOfMonoidActionsAlt extends
     ] (
       monoid: Ɛ.Monoid[M]
     ) : Topos[~] =
-      ???
-//      new Topos[~] {
-//      }
+      new Topos[~] {
+          override type DOT[A <: ~] = ActionDotFacade[A]
+          override type >[A <: ~, B <: ~] = ActionArrowFacade[A, B]
+          override type UNIT = Ɛ.UNIT
+
+          override val I = 
+            ??? 
+            
+          override val omega = 
+            ???
+            
+          override val truth = 
+            ???
+
+          trait ActionDotFacade[
+            S <: ~
+          ] extends Dot[S] 
+            
+          class ActionDot[
+            S <: ~
+          ](
+              action: monoid.Action[S]
+          ) extends ActionDotFacade[S] {
+            override def `>Uncached`[
+              T <: ~
+            ](
+              that: ActionDotFacade[T]
+            ): EXPONENTIAL[S,T] = 
+              ???
+              
+            override def apply[
+              T <: ~
+            ](
+                target: ActionDotFacade[T]
+            )(
+                f: S => T
+            ): S > T = 
+              ???
+              
+            override val globals: Traversable[UNIT > S] = 
+              ???
+              
+            override def sanityTest(): Unit = 
+              ???
+              
+            override val toI: S > UNIT = 
+              ???
+              
+            override def xUncached[
+              T <: ~
+            ](
+                that: ActionDotFacade[T]
+            ): BIPRODUCT[S, T] = 
+              ???
+          }
+          
+          trait ActionArrowFacade[
+            S <: ~,
+            T <: ~
+          ] extends Arrow[S, T] 
+          
+          class ActionArrow[
+            S <: ~,
+            T <: ~
+          ](
+              val source: ActionDotFacade[S],
+              val target: ActionDotFacade[T],
+              val function: S => T
+          ) extends ActionArrowFacade[S, T] {
+            override def \[
+              U <: ~
+            ](
+                monic: U > T
+            ): S > U = 
+              ???
+              
+            override def ?=(
+              that: S > T
+            ): EQUALIZER[S] = 
+              ???
+              
+            override def apply(
+              s: S
+            ): T = 
+              ???
+              
+            override val chi: T > TRUTH = 
+              ???
+              
+            override def o[
+              R <: ~
+            ](
+              that: R > S
+            ): R > T = 
+              ???
+              
+            override def sanityTest() = 
+              ???
+          }
+      }
     }
 }
