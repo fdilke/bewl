e02b754bf07a1f97508016b8d05ccee2078071f2
Felix <fdilke@gmail.com>
2017-09-14 17:51:55 +0100

    streamlined ActionSplitter yet more

--- src/main/scala/com/fdilke/bewl/fsets/monoid_actions/ActionSplitter.scala
+++ src/main/scala/com/fdilke/bewl/fsets/monoid_actions/ActionSplitter.scala
@@ -19,15 +19,15 @@ trait ActionSplitter extends BaseFiniteSets {
     componentAction: ACTION[A]
   )
 
-  trait ActionSplitting[
+  case class ActionSplitting[
     A,
     ACTION[B]
-  ] {
-    val allGenerators: Seq[A]
-    val components: Seq[
+  ] (
+    allGenerators: Seq[A],
+    components: Seq[
       ActionComponent[A, ACTION]
     ]
-  }
+  )
 
   object ActionSplitter {
     def forMonoid[M](
@@ -49,64 +49,59 @@ trait ActionSplitter extends BaseFiniteSets {
 
         def splitAction[A](
           action: monoid.Action[A]
-        ) =
+        ) = {
+          val allGenerators: Seq[A] =
+            findGenerators apply action generators
+          val indexedGenerators: Seq[(A, Int)] =
+            allGenerators.zipWithIndex
+          val actionMultiply: BiArrow[A, M, A] =
+            action.actionMultiply
+          val generatorSorts: Seq[Int] =
+            BuildEquivalence(
+              allGenerators.size,
+              for {
+                (g, i) <- indexedGenerators
+                (h, j) <- indexedGenerators
+                m <- monoidElements
+                n <- monoidElements
+                if actionMultiply(g, m) ==
+                  actionMultiply(h, n)
+              }
+                yield i -> j
+            )
+
           new ActionSplitting[
             A,
             ({type λ[T] = monoid.Action[T]}) # λ
-          ] {
-            override val allGenerators =
-              findGenerators apply action generators
-
-            override val components: Seq[
-              ActionComponent[
-                A,
-                ({type λ[T] = monoid.Action[T]}) # λ
-              ]
-            ] = {
-              val indexedGenerators =
-                allGenerators.zipWithIndex
-              val actionMultiply =
-                action.actionMultiply
-
-              val generatorSorts =
-                BuildEquivalence(
-                  allGenerators.size,
-                  for {
-                    (g, i) <- indexedGenerators
-                    (h, j) <- indexedGenerators
-                    m <- monoidElements
-                    n <- monoidElements
-                    if actionMultiply(g, m) ==
-                      actionMultiply(h, n)
-                  }
-                    yield i -> j
-                )
-
-              allGenerators.indices.groupBy(
-                generatorSorts
-              ).values map { block =>
-                  val componentGenerators: Seq[A] =
-                    block map allGenerators
-                  ActionComponent[
-                    A,
-                    ({type λ[T] = monoid.Action[T]}) # λ
+          ] (
+            allGenerators,
+            allGenerators.indices.groupBy(
+              generatorSorts
+            ).values map { (block: Seq[Int]) =>
+              {
+                val componentGenerators: Seq[A] =
+                  block map allGenerators
+                ActionComponent[
+                  A,
+                  ({type λ[T] = monoid.Action[T]}) # λ
                   ] (
-                      componentGenerators,
-                      monoid.action(
-                        makeDot(
-                          for {
-                            cg <- componentGenerators
-                            m <- monoidElements
-                          } yield
-                            actionMultiply(cg, m)
-                        )
-                      ) (
-                          actionMultiply
-                        )
+                  componentGenerators,
+                  monoid.action(
+                    makeDot(
+                      for {
+                        cg <- componentGenerators
+                        m <- monoidElements
+                      } yield
+                        actionMultiply(cg, m)
+                    )
+                  ) (
+                    actionMultiply
                   )
-              } toSeq
-            }
-          }
+                )
+              }
+            } toSeq
+          )
+        }
       }
   }
 }
