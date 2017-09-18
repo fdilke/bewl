21b89cba4900597121f6b1022e12412d0278f664
Felix Dilke <fdilkeuk@yahoo.co.uk>
2017-09-18 17:27:16 +0100

    refactored the equivalence builder

--- src/main/scala/com/fdilke/bewl/helper/BuildEquivalence.scala
+++ src/main/scala/com/fdilke/bewl/helper/BuildEquivalence.scala
@@ -1,69 +1,45 @@
 package com.fdilke.bewl.helper
 
+import scala.language.postfixOps
+
+// A 0-based, functional tribute to the original Numerical Recipes
+// algorithm: http://www.aip.de/groups/soe/local/numres/bookcpdf/c8-6.pdf
+
 object BuildEquivalence {
+
   def apply(
     size: Int,
     relators: Traversable[(Int, Int)]
   ): Seq[Int] = {
+    val range =
+      0 until size
+
     val array: Array[Int] =
-      (0 until size).toArray
+      range toArray
 
-    var j, k = 0
+    val trackUp: Int => Int =
+      IterateToFixed(_)(array)
 
-    //    for (k<-0 until n) nf(k)=k /* Initialize each element its own class. */
-    for ((jj, kk) <- relators) {
-      /* For each piece of input information... */
-      j = jj
-      while (array(j) != j) j = array(j) /* Track first element up to its ancestor. */
-      k = kk
-
-      while (array(k) != k) k = array(k) /* Track second element up to its ancestor. */
-      if (j != k) array(j) = k /* If they are not already related, make them so */
-    }
-
-    for (j <- 0 until size) /* Final sweep up to highest ancestors. */
-      while (array(j) != array(array(j))) array(j) = array(array(j))
-
-    array
-  }
-/*
-  def apply(
-    size: Int,
-    relators: Traversable[(Int, Int)]
-  ): Seq[Int] = {
-    val array: Array[Int] =
-      (0 until size).toArray
-
-//    def trackUp(i: Int) =
-//      IterateToFixed(i)(array)
-    def trackUp(i: Int) = {
-      var ii = i;
-      while (ii != array(ii))
-        ii = array(ii)
-      ii
+    def equate(
+      i: Int,
+      j: Int
+    ) {
+      if (i != j)
+        array(i) = j
     }
 
     for {
       (j, k) <- relators
-      _ = println(s"ZZZ relator: $j -> $k")
-      jj = trackUp(j)
-      kk = trackUp(k)
-    } 
-      if (jj != kk)
-        array(jj) = kk
-
-// exercise for the student: why doesn't this work?
-//        array map trackUp
-// instead we have to do:
-
-    println("ZZZ Tracking up... array = " + (array.toSeq.mkString(":")))
-    for { i <- 0 until size } {
-      while (array(i) != array(array(i)))
-          array(i) = array(array(i))
     }
-    println("ZZZ Tracking up... done")
+      equate(
+        trackUp(j),
+        trackUp(k)
+      )
+
+    for { i <- range }
+      array(i) =
+          trackUp(array(i))
 
     array
   }
-*/
 }
