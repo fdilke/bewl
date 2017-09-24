b8b8eec252eb26664f7c34c9e742eb8ea17f1c3f
Felix Dilke <fdilkeuk@yahoo.co.uk>
2017-09-24 09:55:44 +0100

    one-line version of MiserlyMeasure

--- src/main/scala/com/fdilke/bewl/helper/MiserlyMeasure.scala
+++ src/main/scala/com/fdilke/bewl/helper/MiserlyMeasure.scala
@@ -7,16 +7,8 @@ object MiserlyMeasure {
   def apply[A](
     sequence: Traversable[A],
     expectedSize: Int
-  ): Boolean = {
-    val expectedEmpty = expectedSize == 0
-    if (sequence.isEmpty)
-      expectedEmpty
-    else if (expectedEmpty)
-      false
-    else
-      MiserlyMeasure(
-        sequence.tail,
-        expectedSize - 1
-      )
-  }
+  ): Boolean =
+    sequence.toSeq.lengthCompare(
+      expectedSize
+    ) == 0
 }
