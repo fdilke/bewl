21e5b1be1a8be20112bde274466a7f45d675d2c9
Felix Dilke <fdilkeuk@yahoo.co.uk>
2023-01-26 20:12:14 +0000

    Refactor Green-Rees to use extensions to Seq[H]

--- bewl2/src/main/scala/com/fdilke/bewl2/algebra/constructions/GreenRees.scala
+++ bewl2/src/main/scala/com/fdilke/bewl2/algebra/constructions/GreenRees.scala
@@ -5,13 +5,25 @@ import scala.annotation.tailrec
 // See green-rees-theory.md
 
 object GreenRees {
-  def factorize[H](word: Seq[H]): Factorization[H] =
-    val allLetters = word.toSet
-    val (leftSegment, leftLetter) = extractLeftSegment(word, allLetters)
-    val (rightSegmentR, rightLetter) = extractLeftSegment(word.reverse, allLetters)
-    Factorization(leftSegment, leftLetter, rightLetter, rightSegmentR.reverse)
 
-  @tailrec private def extractLeftSegmentHelper[H](
+  extension[H](word: Seq[H])
+    def factorize: Factorization[H] =
+      val allLetters: Set[H] = word.toSet
+      val (leftSeg: Seq[H], leftLetter: H) = word.leftSegment(allLetters)
+      val (rightSegR, rightLetter): (Seq[H], H) = word.reverse.leftSegment(allLetters)
+      Factorization(leftSeg, leftLetter, rightLetter, rightSegR.reverse)
+      
+    def leftSegment: (Seq[H], H) =
+      leftSegment(word.toSet)
+
+    def leftSegment(
+      allLetters: Set[H]
+    ): (Seq[H], H) =
+      if word.isEmpty then
+        throw new IllegalArgumentException("can't extract segment from empty word")
+      extractLeftSegmentHelper(word, Seq.empty, word.toSet)
+
+  @tailrec private def extractLeftSegmentHelper[H](  
     segment: Seq[H],
     seen: Seq[H],
     lettersRemaining: Set[H]
@@ -22,19 +34,7 @@ object GreenRees {
       (seen, letter)
     else
       extractLeftSegmentHelper(segment.tail, seen :+ letter, newLettersRemaining)
-
-  def extractLeftSegment[H](
-    word: Seq[H],
-    allLetters: Set[H]
-  ): (Seq[H], H) =
-    if word.isEmpty then
-      throw new IllegalArgumentException("can't extract segment from empty word")
-    extractLeftSegmentHelper(word, Seq.empty, word.toSet)
-
-  def extractLeftSegment[H](
-    word: Seq[H]
-  ): (Seq[H], H) =
-    extractLeftSegment(word, word.toSet)
+    
 
   case class Factorization[H](
     leftSegment: Seq[H],
--- bewl2/src/test/scala/com/fdilke/bewl2/algebra/constructions/GreenReesSpec.scala
+++ bewl2/src/test/scala/com/fdilke/bewl2/algebra/constructions/GreenReesSpec.scala
@@ -6,21 +6,21 @@ class GreenReesSpec extends RichFunSuite:
 
   test("Can extract left segments from a word") {
     intercept[java.lang.IllegalArgumentException]{
-      extractLeftSegment(Seq())
+      Seq[Int]().leftSegment
     }
-    extractLeftSegment(Seq(0)) is (Seq(), 0)
-    extractLeftSegment(Seq(0, 1)) is (Seq(0), 1)
-    extractLeftSegment(Seq(0, 1, 2)) is (Seq(0, 1), 2)
-    extractLeftSegment(Seq(2, 0, 0, 2, 1, 0, 2)) is (Seq(2, 0, 0, 2), 1)
+    Seq(0).leftSegment is (Seq(), 0)
+    Seq(0, 1).leftSegment is (Seq(0), 1)
+    Seq(0, 1, 2).leftSegment is (Seq(0, 1), 2)
+    Seq(2, 0, 0, 2, 1, 0, 2).leftSegment is (Seq(2, 0, 0, 2), 1)
   }
 
   test("Can factorize a word") {
     intercept[java.lang.IllegalArgumentException]{
-      factorize[Int](Seq())
+      Seq[Int]().factorize
     }
-    factorize[Int](Seq(0)) is Factorization(Seq(), 0, 0, Seq())
-    factorize[Int](Seq(0, 1, 2)) is Factorization(Seq(0, 1), 2, 0, Seq(1, 2))
-    factorize[Int](Seq(0, 1, 1, 2)) is Factorization(Seq(0, 1, 1), 2, 0, Seq(1, 1, 2))
-    factorize[Int](Seq(2, 1, 1, 0, 0, 2, 1, 2)) is Factorization(Seq(2, 1, 1), 0, 0, Seq(2, 1, 2))
+    Seq(0).factorize is Factorization(Seq(), 0, 0, Seq())
+    Seq(0, 1, 2).factorize is Factorization(Seq(0, 1), 2, 0, Seq(1, 2))
+    Seq(0, 1, 1, 2).factorize is Factorization(Seq(0, 1, 1), 2, 0, Seq(1, 1, 2))
+    Seq(2, 1, 1, 0, 0, 2, 1, 2).factorize is Factorization(Seq(2, 1, 1), 0, 0, Seq(2, 1, 2))
   }
 
