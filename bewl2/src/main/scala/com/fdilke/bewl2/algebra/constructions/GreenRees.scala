f3e8002bfd1fcde2ce9fb6170f4ce8daf75d289e
Felix Dilke <fdilkeuk@yahoo.co.uk>
2023-02-17 12:01:41 +0000

    recreated 'left segment' function

--- bewl2/src/main/scala/com/fdilke/bewl2/algebra/constructions/GreenRees.scala
+++ bewl2/src/main/scala/com/fdilke/bewl2/algebra/constructions/GreenRees.scala
@@ -1,7 +1,31 @@
 package com.fdilke.bewl2.algebra.constructions
 
+import scala.annotation.tailrec
+import com.fdilke.utility.Shortcuts.*
+
 object GreenRees:
   extension(word: String)
-    def thing: String =
-      ??? 
+    def leftSegment: String =
+      if (word.isEmpty)
+        throw new IllegalArgumentException("can't extract segment from empty word")      
+      else
+        val (prefix, last) = extractLeftSegmentHelper(word, "", word.toSet)
+        (prefix :+ last).string
+
+  @tailrec private def extractLeftSegmentHelper(  
+    segment: Seq[Char],
+    seen: Seq[Char],
+    lettersRemaining: Set[Char]
+  ): (Seq[Char], Char) =
+    val letter = segment.head
+    val newLettersRemaining = lettersRemaining - letter
+    if (newLettersRemaining.isEmpty) then
+      (seen, letter)
+    else
+      extractLeftSegmentHelper(
+        segment.tail,
+        seen :+ letter,
+        newLettersRemaining
+      )
+        
 
--- bewl2/src/test/scala/com/fdilke/bewl2/algebra/constructions/GreenReesSpec.scala
+++ bewl2/src/test/scala/com/fdilke/bewl2/algebra/constructions/GreenReesSpec.scala
@@ -6,8 +6,19 @@ import com.fdilke.bewl2.sets.SetsUtilities.*
 import java.util.concurrent.atomic.AtomicBoolean
 import com.fdilke.utility.Shortcuts._
 import com.fdilke.bewl2.algebra.constructions.Factorization
+import GreenRees.*
 
 class GreenReesSpec extends RichFunSuite:
 
   test("Can extract left segments from a word") {
+    intercept[java.lang.IllegalArgumentException]{
+      "".leftSegment
+    }
+    "a".leftSegment is "a"
+    "ab".leftSegment is "ab"
+    "abc".leftSegment is "abc"
+    "caacbac".leftSegment is "caacb"
+    "abacab".leftSegment is "abac"
+    "bcnofne".leftSegment is "bcnofne"
+    "daguerrotype".leftSegment is "daguerrotyp"
   }
