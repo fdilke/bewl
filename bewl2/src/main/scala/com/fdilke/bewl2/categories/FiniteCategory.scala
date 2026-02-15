fab96a16ebe4cd0a6202899db8bc96c14ee48ecc
Felix Dilke <fdilkeuk@yahoo.co.uk>
2026-02-15 14:12:38 +0000

    verifying arrow sources/targets

--- bewl2/src/main/scala/com/fdilke/bewl2/categories/FiniteCategory.scala
+++ bewl2/src/main/scala/com/fdilke/bewl2/categories/FiniteCategory.scala
@@ -1,10 +1,23 @@
 package com.fdilke.bewl2.categories
 
-trait Arrow[DOT]
+trait PreArrow[DOT]:
+  val name: String
+  val source: DOT
+  val target: DOT
+
 class FiniteCategory[DOT](
   dots: DOT*
 )(
-  arrows: Arrow[DOT]*
+  arrows: PreArrow[DOT]*
 ):
-  def sanityTest(): Unit =
-    ()  
+  for { arrow <- arrows }
+    if (!((dots.contains(arrow.source)) && (dots.contains(arrow.target)))) {
+      throw new IllegalArgumentException("source/target not listed")
+    }
+
+object FiniteCategory:
+  case class SimplePreArrow[DOT](
+    name: String,
+    source: DOT,
+    target: DOT                            
+  ) extends PreArrow[DOT]
--- bewl2/src/test/scala/com/fdilke/bewl2/categories/FiniteCategorySpec.scala
+++ bewl2/src/test/scala/com/fdilke/bewl2/categories/FiniteCategorySpec.scala
@@ -6,13 +6,26 @@ import com.fdilke.bewl2.helper.BuildEquivalence
 import scala.language.{existentials, reflectiveCalls}
 
 class FiniteCategorySpec extends FunSuite:
-  test("Can create the trivial category 1"):
-    val category =
-      FiniteCategory[String]("1")()
-    category.sanityTest()
-
   test("Can create the empty category 1"):
-    val category =
-      FiniteCategory[String]()()
-    category.sanityTest()
+    FiniteCategory[String]()()
+
+  test("Can create the trivial category 1"):
+    FiniteCategory[Int](1)()
   
+  test("requires listing all sources/targets"):
+    intercept[IllegalArgumentException]:
+      FiniteCategory[Int]()(
+        FiniteCategory.SimplePreArrow[Int]("arrow", 1, 2)
+      )
+  intercept[IllegalArgumentException]:
+    FiniteCategory[Int](1)(
+      FiniteCategory.SimplePreArrow[Int]("arrow", 1, 2)
+    )
+  intercept[IllegalArgumentException]:
+    FiniteCategory[Int](2)(
+      FiniteCategory.SimplePreArrow[Int]("arrow", 1, 2)
+    )
+  FiniteCategory[Int](1, 2)(
+    FiniteCategory.SimplePreArrow[Int]("arrow", 1, 2)
+  )
+
